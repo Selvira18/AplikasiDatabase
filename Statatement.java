0f783d2a15641a9aea3e6dc6e1eb98afaa581b6d
Selvira18 <37258333+Selvira18@users.noreply.github.com>
2018-12-11 22:11:24 +0700

    Add files via upload

---
+++ Statatement.java
@@ -0,0 +1,24 @@
+
+import java.sql.ResultSet;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS-NB
+ */
+class Statatement {
+
+    static ResultSet executeUpdate(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void Close() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ konekasi.java
@@ -0,0 +1,26 @@
+
+import java.sql.Statement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS-NB
+ */
+class konekasi {
+
+    static class getConnection {
+
+        static Statement creatStatement() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        public getConnection() {
+        }
+    }
+    
+}
